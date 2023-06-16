700d37fffe7226a0efd86d8825813f32bde86fc9
mak448a <mak448a@gmail.com>
2023-06-16 08:10:45 -0500

    Play sound when changing volume

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import platform
 import subprocess
 
+import playsound
 import pyautogui as pag
 import pygame
 
@@ -79,7 +80,9 @@ while True:
     if platform.system() == "Linux":
         if joystick.get_button(6):  # Left trigger
             subprocess.call(["pactl", "set-sink-volume", "0", "-5%"])
+            playsound.playsound("/usr/share/sounds/freedesktop/stereo/bell.oga")
         if joystick.get_button(7):  # Right trigger
             subprocess.call(["pactl", "set-sink-volume", "0", "+5%"])
+            playsound.playsound("/usr/share/sounds/freedesktop/stereo/bell.oga")
 
     pag.moveTo(coordinate)
